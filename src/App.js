9952a0bb23d3663bc8d149d5b0b5f85d9278dfd2
Peter <Posoo617@gmail.com>
2020-08-30 07:02:47 +0300

    Setup controlled components - Form Inputs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
-import React from "react"
-import logo from "./logo.svg"
+import React, { useState } from "react"
 import "./App.css"
 
-function App() {
+const App = () => {
+	const [value, setValue] = useState({
+		firstName: "",
+		lastName: "",
+		email: "",
+	})
+
+	const handleFirstNameInputChange = event => {
+		setValue({ ...value, firstName: event.target.value })
+	}
+	const handleLastNameInputChange = event => {
+		setValue({ ...value, lastName: event.target.value })
+	}
+	const handleEmailNameInputChange = event => {
+		setValue({ ...value, email: event.target.value })
+	}
+
 	return (
-		<div className="App">
-			<header className="App-header">
-				<h3>React Forms</h3>
-			</header>
+		<div class="form-container">
+			<form class="register-form">
+				<input
+					value={value.firstName}
+					onChange={handleFirstNameInputChange}
+					id="first-name"
+					class="form-field"
+					type="text"
+					placeholder="First Name"
+					name="firstName"
+				/>
+
+				<input
+					value={value.lastName}
+					onChange={handleLastNameInputChange}
+					id="last-name"
+					class="form-field"
+					type="text"
+					placeholder="Last Name"
+					name="lastName"
+				/>
+
+				<input
+					value={value.email}
+					onChange={handleEmailNameInputChange}
+					id="email"
+					class="form-field"
+					type="text"
+					placeholder="Email"
+					name="email"
+				/>
+
+				<button class="form-field" type="submit">
+					Register
+				</button>
+			</form>
 		</div>
 	)
 }
